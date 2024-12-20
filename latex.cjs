43bdbac81b3c42c21ea9b4560f802d5d0f0c2571
Jacob Gantt <contact@jgantts.com>
2024-12-19 19:31:33 -0500

    tex

---
+++ latex.cjs
@@ -0,0 +1,23 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+
+// Config
+const TEXDIR = "./documents/tex"
+const OUTDIR = `./latex-aux`;
+const TEXFILE = `./jgantts-conlangIII.tex`;
+
+if (!fs.existsSync(OUTDIR)) {
+  fs.mkdirSync(OUTDIR, { recursive: true });
+}
+
+// Build Command
+const command = `xelatex -output-directory=${OUTDIR} ${TEXFILE}`;
+try {
+  console.log(`Running: ${command}`);
+  execSync(command, { stdio: "inherit", cwd: TEXDIR });
+  console.log("Build complete.");
+} catch (error) {
+  console.error("Build failed:", error.message);
+  process.exit(1);
+}
