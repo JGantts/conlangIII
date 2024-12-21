717d75cee2edfbef5f43fdf8c4990ac36434ae06
Jacob Gantt <contact@jgantts.com>
2024-12-21 00:46:23 -0500

    Update Dictionary.vue

--- src/Dictionary.vue
+++ src/Dictionary.vue
@@ -1,64 +1,86 @@
 <template>
-      <h1>Dictionary</h1>
-    <div class="dictionary">
-      <div v-for="entry in dictionaryData" :key="entry.lexeme" class="dictionary-entry">
-        <h2>{{ entry.lexeme }}</h2>
-        <h4>{{ entry.simple }}</h4>
-        <ul class="definitions-list">
-          <li v-for="(definition, index) in entry.defs" :key="index" class="definition-holder">
-            <div class="definition">
-                <div class="definition-header">
-                    <span style="font-style: bold;">{{ index+1 }}</span>
-                    <span>&nbsp;</span>
-                    <span v-if="definition.type.type=='noun'" style="font-style: italic;">n.</span>
-                    <span v-if="definition.type.type=='verb'" style="font-style: italic;">v.</span>
-                    <span>&nbsp;</span>
-                    <span>{{ definition.def }}</span>
-                </div>
-                <template v-if="definition.ex && definition.ex.length > 0">
-                <div class="examples-container">
-                    <ul class="examples-list">
-                    <li v-for="(example, exIndex) in definition.ex" :key="exIndex" class="example">
-                        <div class="example-lang">{{ example.ex }}</div>
-                        <!--
-                        <div><strong>Gloss I:</strong> {{ example.glossI || "N/A" }}</div>
-                        <div><strong>Gloss II:</strong> {{ example.glossII || "N/A" }}</div>
-                        -->
-                        <div class="english">"{{ example.eng }}"</div>
-                    </li>
-                    </ul>
-                </div>
-                </template>
+  <h1>Dictionary</h1>
+  <div class="dictionary">
+    <div
+      v-for="entry in dictionaryData"
+      :key="entry.lexeme"
+      class="dictionary-entry"
+    >
+      <h2>{{ entry.lexeme }}</h2>
+      <h4>{{ entry.simple }}</h4>
+      <ul class="definitions-list">
+        <li
+          v-for="(definition, index) in entry.defs"
+          :key="index"
+          class="definition-holder"
+        >
+          <div class="definition">
+            <div class="definition-header">
+              <span style="font-weight: bold;">{{ index + 1 }}</span>
+              <span>&nbsp;</span>
+              <span
+                v-if="definition.type.type === 'noun'"
+                style="font-style: italic;"
+              >
+                n.
+              </span>
+              <span
+                v-if="definition.type.type === 'verb'"
+                style="font-style: italic;"
+              >
+                v.
+              </span>
+              <span>&nbsp;</span>
+              <span>{{ definition.def }}</span>
             </div>
-          </li>
-        </ul>
-      </div>
+            <template v-if="definition.ex && definition.ex.length > 0">
+              <div class="examples-container">
+                <ul class="examples-list">
+                  <li
+                    v-for="(example, exIndex) in definition.ex"
+                    :key="exIndex"
+                    class="example"
+                  >
+                    <div class="example-lang">{{ example.ex }}</div>
+                    <!--
+                      <div><strong>Gloss I:</strong> {{ example.glossI || "N/A" }}</div>
+                      <div><strong>Gloss II:</strong> {{ example.glossII || "N/A" }}</div>
+                    -->
+                    <div class="english">"{{ example.eng }}"</div>
+                  </li>
+                </ul>
+              </div>
+            </template>
+          </div>
+        </li>
+      </ul>
     </div>
-  </template>
-  
-  <script>
-  import { defineComponent } from "vue";
-  import dictionaryData from "./dictionary.json"; // Import the JSON file
-  
-  console.log(dictionaryData)
+  </div>
+</template>
 
-  export default defineComponent({
-    name: "DictionaryDisplay",
-    setup() {
-      return {
-        dictionaryData,
-      };
-    },
-  });
-  </script>
-  
-  <style scoped>
+<script>
+import { defineComponent } from "vue";
+import dictionaryData from "./dictionary.json";
+
+console.log(dictionaryData);
+
+export default defineComponent({
+  name: "DictionaryDisplay",
+  setup() {
+    return {
+      dictionaryData,
+    };
+  },
+});
+</script>
+
+<style scoped>
 /* General Styles */
 .dictionary {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    align-items: flex-start;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: flex-start;
   font-family: inherit;
   color: var(--text-color);
   background-color: var(--bg-color);
@@ -79,7 +101,7 @@ h1 {
 
 /* Entry Styles */
 .dictionary-entry {
-    position: relative;
+  position: relative;
   border: none;
   background-color: var(--bg-color);
   padding: 20px 20px 0 20px;
@@ -94,7 +116,7 @@ h1 {
 }
 
 .dictionary-entry h2 {
-    margin: 0;
+  margin: 0;
   font-size: 2rem;
   font-weight: bold;
   text-transform: lowercase;
@@ -102,40 +124,44 @@ h1 {
 }
 
 .dictionary-entry::before {
-    content: "";
-    position: absolute;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    height: 40px; /* Adjust the height of the gradient */
-    background: linear-gradient(to bottom, rgba(255, 255, 255, 0) 0%, var(--bg-color) 100%);
-    pointer-events: none; /* Ensures the pseudo-element doesn't interfere with user interaction */
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 40px;
+  background: linear-gradient(
+    to bottom,
+    rgba(255, 255, 255, 0) 0%,
+    var(--bg-color) 100%
+  );
+  pointer-events: none;
 }
 
 .dictionary-entry:hover::before {
-    background: none;
+  background: none;
 }
 
 .dictionary-entry::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
   border: 1px solid var(--text-color-faded);
   border-radius: 10px;
 }
 
 .dictionary-entry .definitions-list {
-    height: 40px;
-    overflow: hidden;
-    transition: height 0.3s;
+  height: 40px;
+  overflow: hidden;
+  transition: height 0.3s;
 }
 
 .dictionary-entry:hover .definitions-list {
-    height: 100px;
-    overflow: visible;
+  height: 100px;
+  overflow: visible;
 }
 
 /* Definitions List */
@@ -156,15 +182,14 @@ h1 {
 }
 
 .dictionary-entry:hover .definitions-list {
-    position: relative;
-    z-index: 10;
+  position: relative;
+  z-index: 10;
 }
 
 .definition:hover .def-elipsis {
-    opacity: 0;
-    transition: opacity 0.3s;
+  opacity: 0;
+  transition: opacity 0.3s;
 }
-
 
 .definition:first-child {
   border-top: none; /* Remove line above the first definition */
@@ -217,11 +242,11 @@ h4 {
 }
 
 .example-lang {
-    font-weight: 100;
+  font-weight: 100;
 }
 
 .english {
-    font-weight: 100;
-    font-style: italic;
+  font-weight: 100;
+  font-style: italic;
 }
 </style>
